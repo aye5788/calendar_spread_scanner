5dbde1b6372f930327a8ad37fe0aabca73768c0c
aye5788 <166234294+aye5788@users.noreply.github.com>
2025-11-17 09:24:44 -0500

    Add colorize function for value categorization

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,8 @@
+def colorize(value, good_threshold, bad_threshold):
+    if value >= good_threshold:
+        return "🟢 " + str(round(value, 3))
+    elif value <= bad_threshold:
+        return "🔴 " + str(round(value, 3))
+    else:
+        return "🟡 " + str(round(value, 3))
 
