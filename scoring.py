a4c711648a075a4a22267a103dc0b8f139ecf785
aye5788 <166234294+aye5788@users.noreply.github.com>
2025-11-17 09:39:39 -0500

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,22 +1,16 @@
 def score_calendar(m):
-
     score = 0
 
     if m["vtr"] and m["vtr"] > 3:
         score += 2
-
     if m["em_ratio"] and m["em_ratio"] > 1.1:
         score += 2
-
     if m["peak_cost_ratio"] and m["peak_cost_ratio"] > 2:
         score += 2
-
     if m["iv_decay_diff"] > 0:
         score += 1
-
     if m["dte_ratio"] and 3 <= m["dte_ratio"] <= 6:
         score += 1
-
     if m["net_gamma"] < 0.005:
         score += 1
 
