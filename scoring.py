d13f1f403fbf50401b4f8950abc1d145cfa7500b
aye5788 <166234294+aye5788@users.noreply.github.com>
2025-11-17 09:24:25 -0500

    Implement scoring logic in score_calendar function

--- scoring.py
+++ scoring.py
@@ -1,1 +1,26 @@
+def score_calendar(metrics):
+    score = 0
+
+    # Weighting logic
+    if metrics["vtr"] and metrics["vtr"] > 3:
+        score += 2
+    elif metrics["vtr"] > 1:
+        score += 1
+
+    if metrics["em_ratio"] and metrics["em_ratio"] > 1.1:
+        score += 2
+
+    if metrics["peak_cost_ratio"] and metrics["peak_cost_ratio"] > 2:
+        score += 2
+
+    if metrics["iv_decay_diff"] > 0:
+        score += 1
+
+    if metrics["dte_ratio"] and 3 <= metrics["dte_ratio"] <= 6:
+        score += 1
+
+    if metrics["net_gamma"] < 0.005:
+        score += 1
+
+    return score
 
