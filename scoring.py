4d6d50d645d9bbac0dfd8c8d9350329b4674f22c
aye5788 <166234294+aye5788@users.noreply.github.com>
2025-11-17 09:30:58 -0500

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,26 +1,23 @@
-def score_calendar(metrics):
+def score_calendar(m):
+
     score = 0
 
-    # Weighting logic
-    if metrics["vtr"] and metrics["vtr"] > 3:
-        score += 2
-    elif metrics["vtr"] > 1:
-        score += 1
-
-    if metrics["em_ratio"] and metrics["em_ratio"] > 1.1:
+    if m["vtr"] and m["vtr"] > 3:
         score += 2
 
-    if metrics["peak_cost_ratio"] and metrics["peak_cost_ratio"] > 2:
+    if m["em_ratio"] and m["em_ratio"] > 1.1:
         score += 2
 
-    if metrics["iv_decay_diff"] > 0:
+    if m["peak_cost_ratio"] and m["peak_cost_ratio"] > 2:
+        score += 2
+
+    if m["iv_decay_diff"] > 0:
         score += 1
 
-    if metrics["dte_ratio"] and 3 <= metrics["dte_ratio"] <= 6:
+    if m["dte_ratio"] and 3 <= m["dte_ratio"] <= 6:
         score += 1
 
-    if metrics["net_gamma"] < 0.005:
+    if m["net_gamma"] < 0.005:
         score += 1
 
     return score
-
